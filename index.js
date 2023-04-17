7b6c3c2fadb90453782662e4399706ce8d03a5f2
Rayane Marques <125264099+DevRayaneMarques@users.noreply.github.com>
2023-04-17 16:55:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// Importações principais e variáveis de ambiente
+require("dotenv").config();
+const express = require("express");
+
+// Configuração do App
+const app = express();
+app.use(express.json()); // Possibilitar transitar dados usando JSON
+
+// Configuração do Banco de Dados
+const { connection, authenticate } = require("./database/database");
+authenticate(connection); // efetivar a conexão
+const Cliente = require("./database/cliente"); // Configurar o model da aplicação
+const Endereco = require("./database/endereco");
+
+// Definição de rotas
+app.post("/clientes", async (req, res) => {
+  // Coletar os dados do req.body
+  const { nome, email, telefone, endereco } = req.body;
+
+  try {
+    // Dentro de 'novo' estará o o objeto criado
+    const novo = await Cliente.create(
+      { nome, email, telefone, endereco },
+      { include: [Endereco] } // Permite inserir cliente e endereço num comando só
+    );
+    res.status(201).json(novo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+// Escuta de eventos (listen)
+app.listen(3000, () => {
+  // Gerar as tabelas a partir do model
+  // Force = apaga tudo e recria as tabelas
+  connection.sync({ force: true });
+  console.log("Servidor rodando em http://localhost:4000/");
+});
